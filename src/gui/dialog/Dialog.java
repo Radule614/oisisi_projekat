534c02c52e3d6408bc512d2a2c0f31e5f5912cc7
Rade Stojanovic <stojanovicrade614@gmail.com>
2021-12-01 16:49:21 +0100

    table panel class moved

--- src/gui/dialog/Dialog.java
+++ src/gui/dialog/Dialog.java
@@ -1,7 +1,10 @@
 package gui.dialog;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -18,6 +21,7 @@ import javax.swing.border.EmptyBorder;
 import app.Settings;
 import gui.MainWindow;
 import gui.dialog.Dialog.DialogTab.DialogPanel;
+import gui.table.Table;
 
 public abstract class Dialog extends JDialog {
 	private static final long serialVersionUID = -1676308500879210531L;
@@ -353,9 +357,68 @@ public abstract class Dialog extends JDialog {
 				return comboBox;
 			}
 		}
-		
 	}
 	
+	public class TablePanel extends JPanel{
+		private static final long serialVersionUID = 1733942904388036989L;
+		
+		Table table;
+		
+		ArrayList<JPanel> panels = new ArrayList<JPanel>();
+		ArrayList<JButton> buttons = new ArrayList<JButton>();
+		
+		JPanel buttonPanel;
+		
+		public TablePanel(Table table)
+		{
+			super();
+			this.table = table;
+			this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
+			
+			this.setPanels();
+		}
+		
+		protected void setPanels()
+		{
+			this.setButtonPanel();
+			this.setTable();
+			
+			for(JPanel panel: panels) this.add(panel);
+		}
+		
+		protected void setButtonPanel()
+		{
+			JPanel panel = new JPanel();
+			panel.setLayout(new GridLayout(1, 1));
+			JPanel buttonContainer = new JPanel();
+			buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.LINE_AXIS));
+			panel.add(buttonContainer);
+			
+			this.buttonPanel = buttonContainer;
+			this.panels.add(panel);
+		}
+		
+		public void addButton(JButton btn)
+		{
+			this.buttonPanel.add(btn);
+			this.buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
+		}
+		
+		protected void setTable()
+		{
+			this.table.getTableHeader().setFont(new Font("Monaco", Font.PLAIN, 13));
+			this.table.setFont(new Font("Monaco", Font.PLAIN, 13));
+			
+			JPanel panel = new JPanel();
+			panel.setLayout(new BorderLayout());
+			JScrollPane sp = new JScrollPane(this.table);
+			sp.setBorder(new CompoundBorder(new EmptyBorder(15, 0, 25, 0), sp.getBorder()));
+			panel.add(sp, BorderLayout.CENTER);
+			
+			this.panels.add(panel);
+		}
+		
+	}
 }
 
 
--- src/gui/dialog/MultiTabDialog.java
+++ src/gui/dialog/MultiTabDialog.java
@@ -1,16 +1,7 @@
 package gui.dialog;
 
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.util.ArrayList;
-
 import javax.swing.*;
-import javax.swing.border.CompoundBorder;
 import javax.swing.border.EmptyBorder;
-
-import gui.table.Table;
 
 public class MultiTabDialog extends Dialog {
 	private static final long serialVersionUID = 7200065442734182446L;
@@ -36,66 +27,7 @@ public class MultiTabDialog extends Dialog {
 		this.pack();
 	}
 	
-	public class TablePanel extends JPanel{
-		private static final long serialVersionUID = 1733942904388036989L;
-		
-		Table table;
-		
-		ArrayList<JPanel> panels = new ArrayList<JPanel>();
-		ArrayList<JButton> buttons = new ArrayList<JButton>();
-		
-		JPanel buttonPanel;
-		
-		public TablePanel(Table table)
-		{
-			super();
-			this.table = table;
-			this.setLayout(new BoxLayout(this, BoxLayout.PAGE_AXIS));
-			
-			this.setPanels();
-		}
-		
-		protected void setPanels()
-		{
-			this.setButtonPanel();
-			this.setTable();
-			
-			for(JPanel panel: panels) this.add(panel);
-		}
-		
-		protected void setButtonPanel()
-		{
-			JPanel panel = new JPanel();
-			panel.setLayout(new GridLayout(1, 1));
-			JPanel buttonContainer = new JPanel();
-			buttonContainer.setLayout(new BoxLayout(buttonContainer, BoxLayout.LINE_AXIS));
-			panel.add(buttonContainer);
-			
-			this.buttonPanel = buttonContainer;
-			this.panels.add(panel);
-		}
-		
-		public void addButton(JButton btn)
-		{
-			this.buttonPanel.add(btn);
-			this.buttonPanel.add(Box.createRigidArea(new Dimension(10, 0)));
-		}
-		
-		protected void setTable()
-		{
-			this.table.getTableHeader().setFont(new Font("Monaco", Font.PLAIN, 13));
-			this.table.setFont(new Font("Monaco", Font.PLAIN, 13));
-			
-			JPanel panel = new JPanel();
-			panel.setLayout(new BorderLayout());
-			JScrollPane sp = new JScrollPane(this.table);
-			sp.setBorder(new CompoundBorder(new EmptyBorder(15, 0, 25, 0), sp.getBorder()));
-			panel.add(sp, BorderLayout.CENTER);
-			
-			this.panels.add(panel);
-		}
-		
-	}
+	
 }
 
 
